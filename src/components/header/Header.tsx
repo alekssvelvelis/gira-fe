7b229dafa8e1466352c552014a410ac4a81a750b
alekssvelvelis <alekssvelvelis@gmail.com>
2026-04-28 12:51:33 +0300

    feat/file structure

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+
+
+
+export const Header = () => {
+    return (
+        <>
+            <div className="min-w-screen bg-blue-500 h-24">
+                <div className="w-full h-full text-center">
+                    <h1 className="color-red">Test text</h1>
+                </div> 
+            </div>
+        </>
+    )
+}
